a674f90bd35c8393e60f83cd65d4d0b96bbaae38
Daniel-byte628 <danigutierritos2817@gmail.com>
2024-02-17 21:39:24 -0500

    fadfas

---
+++ commands.sh
@@ -0,0 +1,19 @@
+
+
+#3M9c91ELPuVV4f6MHLry6xft2h4i8nb74AgG5DUK7MrX
+#BRbrEH1A9saBNqkCJeb93MzVYDTHYYsj5GamQWRCEZsm
+solana-keygen new --outfile /home/daniel/KeyStrokes/nft/wallets/Owner.json
+
+#2u9H6N6YrJBphuZiJ2KxQzbqoXJSWSiaZbR8uGrDFhby
+#4p8atFTraTjiBoPP7P5Q96ok9k8XV6rAZbawBaMfqXQA
+solana-keygen new --outfile /home/daniel/KeyStrokes/nft/wallets/Treasury.json
+
+#9TRXKsSW9cUHA2qbnVg4weioV7tLsRoJS8PJBPAj5awU
+# F9j6t3XTzPFwXxnajiHyzBYHKY5D8yddf2bNGsyf5RAn
+solana-keygen new --outfile /home/daniel/KeyStrokes/nft/wallets/Creator.json
+
+solana config set --keypair /mnt/c/Users/danig/Desktop/Hackaton/Solana-Hackathon/wallets/Owner.json
+
+#9Lef1VxTQdqYYPHPtL9KQSDj6q7kpoTeNSiFgEpnjFGR
+
+
